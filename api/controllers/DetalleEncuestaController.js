8c0a1eb899387fdecefa97c615264aa71286a51b
luis <lomejordejr@riseup.net>
2017-08-16 16:38:42 -0500

    Agregado base detalleEncuesta

---
+++ api/controllers/DetalleEncuestaController.js
@@ -0,0 +1,11 @@
+/**
+ * DetalleEncuestaController
+ *
+ * @description :: Server-side logic for managing Detalleencuestas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
