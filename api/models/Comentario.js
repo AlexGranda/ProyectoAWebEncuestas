223d2e7452a1ea358d92a951ca4f96adf76e8344
luis <lomejordejr@riseup.net>
2017-08-16 13:55:43 -0500

    Agregado modelo comentario

---
+++ api/models/Comentario.js
@@ -0,0 +1,25 @@
+/**
+ * Comentario.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id:{
+      type:'integer',
+      unique: true,
+      primaryKey: true,
+    },
+    fechaCreacion: {
+      type: 'date',
+      required: true
+    },
+    texto: {
+      type: 'string',
+      required: true
+    }
+  }
+};
