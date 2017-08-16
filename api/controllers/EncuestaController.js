cf315b09c128c0d9b78e9acf5cededd0c3d63e97
luis <lomejordejr@riseup.net>
2017-08-16 13:59:10 -0500

    Agregado modelo encuesta

---
+++ api/controllers/EncuestaController.js
@@ -0,0 +1,11 @@
+/**
+ * EncuestaController
+ *
+ * @description :: Server-side logic for managing Encuestas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Encuesta.js
@@ -0,0 +1,34 @@
+/**
+ * Encuesta.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id:{
+      type:'integer',
+      unique: true,
+      primaryKey: true,
+    },
+    titulo: {
+      type: 'string',
+      required: true
+    },
+    descripcion: {
+      type: 'string',
+      required: true
+    },
+    imagen: {
+      type: 'binary',
+    },
+    likes: {
+      type: 'integer'
+    },
+    dislikes: {
+      type: 'integer'
+    }
+  }
+};
