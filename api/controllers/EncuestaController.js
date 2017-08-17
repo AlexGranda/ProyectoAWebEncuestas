5acccb7c57ced4207c564cd2948afd6d855d2860
Alexandra Granda <Rainini1@users.noreply.github.com>
2017-08-17 10:50:40 -0500

    Eliminar encuesta, crear metodo de editar encuesta

--- api/controllers/EncuestaController.js
+++ api/controllers/EncuestaController.js
@@ -6,6 +6,26 @@
  */
 
 module.exports = {
-	
+  crearEncuesta: function (req, res) {
+    parametros = req.allParams();
+
+    if (re.method == "POST" && parametros.id) {
+      Usuario.destroy({
+        id: params.id
+      }).exec(function (err, usuarioDestruidoRawr) {
+        if (err) return res.negotiate(err);
+        return res.view('encuestas')
+      })
+    }
+    else {
+      return res.badRequest();
+    }
+  },
+  editarEncuesta: function (req, res) {
+    parametros = req.allParams();
+
+    
+
+  }
 };
 
