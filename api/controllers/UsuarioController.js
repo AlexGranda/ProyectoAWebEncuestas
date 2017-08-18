aba4c6035c2efddb8bbfc24d36fa32f267a77018
luis <lomejordejr@riseup.net>
2017-08-18 11:45:28 -0500

    Refactor crear usuario

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -10,7 +10,10 @@ module.exports = {
   crearUsuario: function (req, res) {
 
     var parametros = req.allParams();
-    sails.log.info("parametros", parametros);
+    sails
+      .log
+      .info("parametros", parametros);
+
     var nuevoUsuario = {
       nombres: parametros.nombres,
       apellidos: parametros.apellidos,
@@ -19,43 +22,30 @@ module.exports = {
       fechaNacimiento: parametros.fechaNacimiento
     };
 
-    Usuario.create(nuevoUsuario).exec(function (error, usuarioCreado) {
-      if (error) {
-        return res.serverError(error);
-      }
-      else {
-        Usuario.find().exec(function (error, records) {
-          if (error)
-            return res.serverError(error);
-          else {
-
-            Encuesta.find().exec(function (error, encuestasEncontradas) {
-
-              res.cookie('idUsuario', usuarioCreado.id);
-
-              return res.view('encuestas', {
-        					encuestas: encuestasEncontradas,
-                  usuario: nuevoUsuario
-        			});
-        		})
-          }
-        });
-      }
-    });
+    Usuario
+      .create(nuevoUsuario)
+      .exec(function (error, usuarioCreado) {
+        if (error) {
+          return res.serverError(error);
+        } else {
+          res.cookie('idUsuario', usuarioCreado.id);
+          return res.redirect('/encuestas');
+        }
+      });
   },
 
   llamarVistaPerfil: function (req, res) {
     parametros = req.allParams();
 
-    if(parametros.id)
-    {
-      Usuario.findOne({
-        id:parametros.id
-      }).exec(function (err, usuarioEncontrado) {
-        if(err) return res.serverError(err)
+    if (parametros.id) {
+      Usuario
+        .findOne({id: parametros.id})
+        .exec(function (err, usuarioEncontrado) {
+          if (err)
+            return res.serverError(err)
 
-        return res.view('perfil', {usuario:usuarioEncontrado})
-      })
+          return res.view('perfil', {usuario: usuarioEncontrado})
+        })
     }
   }
 
